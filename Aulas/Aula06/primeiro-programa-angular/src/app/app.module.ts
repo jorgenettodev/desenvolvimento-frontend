63ddf3fb9c6fa3ecb4d9123b8cb31f35f07ce69c
Jorgenettodev <jorgenetto96@gmail.com>
2024-09-02 07:00:27 -0300

    primeira app com angular

---
+++ Aulas/Aula06/primeiro-programa-angular/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/views/home/home.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { LoginComponent } from './components/views/login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
